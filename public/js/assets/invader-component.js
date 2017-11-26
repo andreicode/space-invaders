5b9d2b304f95ffd773bbb8abf8d363214ca2554a
tpetru <tpetru@users.noreply.github.com>
2017-11-26 23:39:41 +0200

    invaders-2 added

--- public/js/assets/invader-component.js
+++ public/js/assets/invader-component.js
@@ -3,7 +3,7 @@ Assets.register({
     init: function (options) {
 
         var geometry = new THREE.BoxGeometry( 1, 1, 1);
-        var material = new THREE.MeshLambertMaterial( { color: 0xFF0000} );
+        var material = new THREE.MeshLambertMaterial( { color: 0xFF0000, } );
         var mesh = new THREE.Mesh(geometry, material);
 
         this.object.add(mesh);
--- public/js/config.js
+++ public/js/config.js
@@ -3,6 +3,7 @@ var __CONFIG = {
     'canvas-height': 600,
     'color-primary': 0x00BA66,
     'color-accent': 0x71F79F,
+    'color-enemy2': 0x8AD48C,
     'font-size': 11,
     'font-size-header': 14,
 }
