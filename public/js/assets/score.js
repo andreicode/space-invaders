41463825a36c43600585860d4342b9a036f97446
tpetru <tpetru@users.noreply.github.com>
2017-11-27 18:08:31 +0200

    score fixed - final

--- public/js/assets/score.js
+++ public/js/assets/score.js
@@ -2,16 +2,27 @@ Assets.register({
     name: 'score',
     init: function(options) {
 
-
         this.object._store = {};
         this.object._store.points = 0;
-        this.object._store.score = Assets.create('text-label', {
-            color: __CONFIG['color-primary'],
-            text: options.text,
-            type: 'normal',
-        });
 
-        this.object.add(this.object._store.score);
+        this.object._store.labels = [];
+        var position = -30;
+
+        for(var i = 0; i < options.labels.length; i++) {
+            this.object._store.labels[i] = {
+                text: options.labels[i]['text'],
+                center: true,
+                object: Assets.create('text-label', {
+                    text: options.labels[i]['text'],
+                    color: __CONFIG['color-primary'],
+                }),
+            };
+
+
+            this.object._store.labels[i].object.position.x = position;
+            this.object.add(this.object._store.labels[i].object);
+            position += 60;
+        }
     },
     update: function() {
         
