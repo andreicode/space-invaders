c7a40be94ecf6426933f3de172c24dcede4afbfc
andreicode <andreipopa.1992@gmail.com>
2017-11-27 17:40:34 +0200

    update

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ var __canvas = document.querySelector('.display__canvas');
 /**
 * Parameters
 */
-var BACKGROUND_COLOR = 0x404040;
+var BACKGROUND_COLOR = 0x2b2b2b;
 var WIDTH = __canvas.clientWidth;
 var HEIGHT = __canvas.clientHeight;
 
@@ -17,6 +17,7 @@ var HEIGHT = __canvas.clientHeight;
  * Handlers
  */
 var __camera = undefined;
+var __controls = undefined;
 var __renderer = undefined;
 
 
@@ -31,6 +32,10 @@ var cube = undefined;
 */
 function createCamera() {
     return new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 0.1, 1000 );
+}
+
+function createControls(camera) {
+    return new THREE.OrbitControls(camera);
 }
 
 function createRenderer() {
@@ -48,13 +53,20 @@ function createRenderer() {
 
 function init() {
     __camera = createCamera();
+    __controls = createControls(__camera);
     __renderer = createRenderer();
 }
 
 function start() {
     // Aditional Set-up ...
     __camera.position.z = 100;
-    Scene.load('game');
+    __camera.position.y = -70;
+    __controls.enableZoom = false;
+    __controls.enablePan = false;
+    __controls.enableRotation = false;
+
+
+    Scene.load('main-menu');
     game();
 }
 
@@ -65,6 +77,7 @@ function start() {
 function game() {
     requestAnimationFrame(game);
     Scene.active._update();
+    __controls.update();
     __renderer.render(Scene.active.scene, __camera);
 }
 
--- public/js/scenes/game.js
+++ public/js/scenes/game.js
@@ -1,32 +1,84 @@
 Scene.register({
     name: 'game',
     init: function () {
+
+        this.scene._store = {};
+
         var group = Assets.create('invader-group');
         group.position.x = -115;
-        group.position.y = 20;
+        group.position.y = 30;
         this.scene.add(group);
+        this.scene._store.group = group;
 
 
         var turret = Assets.create('turret');
-        turret.position.y = -100;
+        turret.position.x = -7;
+        turret.position.y = -85;
+        this.scene.add(turret);
+        this.scene._store.turret = turret;
 
 
-        for (var i = 0; i < 4; i+) {
+        var posy = -60;
+        var posx = -100;
+        for (var i = 0; i < 4; i++) {
             var building = Assets.create('building');
-
-
+            building.position.x = posx;
+            building.position.y = posy;
             this.scene.add(building);
+            posx += 60;
         }
 
         // var score = Assets.create('score');
 
-
-
-        this.scene.add(turret);
         // this.scene.add(score);
-        this.scene.scale.set(0.6,0.6,0.6);
+
+
+
+
+
+        // this.scene.add(score);
+        this.scene.scale.set(0.7,0.7,0.7);
+
+        var light = new THREE.PointLight( 0xffffff, 1, 200 );
+        light.position.set(0, -50, 50);
+        this.scene.add(light);
+
+
+
+        this.scene._store.time = 0;
+        this.scene._store.direction = 0;
+
     },
     update: function () {
+        if(Input.isPressed('Escape')) {
+            Scene.load('main-menu');
+        }
 
+        if(Input.isPressed('ArrowLeft')) {
+            this.scene._store.turret.position.x -= 4;
+        }
+
+        if(Input.isPressed('ArrowRight')) {
+            this.scene._store.turret.position.x += 4;
+        }
+
+        if (this.scene._store.time === 100) {
+            if (this.scene._store.direction === 0) {
+
+
+                this.scene._store.direction = 1;
+            } else {
+
+                this.scene._store.direction = 0;
+            }
+            this.scene._store.time = 0;
+        }
+
+    
+        this.scene._store.time += 1;
+        if(this.scene._store.time  === 15) {
+            this.scene._store.group.position.x += this.scene._store.direction === 0 ? -10 : 10;
+            this.scene._store.time = 0;
+        }
     }
 });
